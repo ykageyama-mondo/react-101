ab9f684045f0b8c4e601b53deab2b07c6ccaf6c2
Yuichi <yuichi.kageyama@mondo.com.au>
2023-03-22 12:27:46 +1100

    Added resource section

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import {WhatIsReact} from './topics/1.WhatIsReact'
 import {HowDoesReactKnowWhenToRender} from './topics/3.HowDoesReactRender'
 import {WhatIsState} from './topics/2.WhatIsState'
 import {Walkthrough} from './topics/4.Walkthrough'
+import {Topic} from './helpers/topic'
+import {Link} from './helpers/link'
+
 function App() {
   return (
     <div className="flex flex-col gap-8">
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center select-none">
         <img alt='react icon' src="../public/React-icon.svg" className="h-14"/>
         <h1 className='pl-2 text-5xl font-thin tracking-tighter text-slate-600'>React <span className='font-mono font-normal text-sky-600'>101</span></h1>
       </div>
 
+      <Topic title="Some resources">
+        <p>This repo is in Github: <Link href='https://github.com/ykageyama-mondo/react-101'>https://github.com/ykageyama-mondo/react-101</Link></p>
+        <p>The React Docs are great and finally released: <Link href='react.dev'>react.dev</Link></p>
+      </Topic>
       <WhatIsReact/>
       <WhatIsState/>
       <HowDoesReactKnowWhenToRender/>
