ea05c41234edff09f4b1cbf366ae30a5eec6e8a6
Yuichi <yuichi.kageyama@mondo.com.au>
2023-03-22 12:28:44 +1100

    remove click listener

--- src/topics/4.Walkthrough.tsx
+++ src/topics/4.Walkthrough.tsx
@@ -6,7 +6,6 @@ const Chat = () => {
   const {messages, sendMessage} = useWebsocket()
 
   useEffect(() => {console.log(messages)}, [messages])
-  const click = () => sendMessage('dwaokdwa')
   return (
     <div className="h-[600px] flex flex-col">
       <div className='border p-5 border-4 border-slate-500 rounded-md flex-grow'>
@@ -15,7 +14,7 @@ const Chat = () => {
       </div>
       <div className='flex gap-2 pt-2'>
         <input className="flex-grow border border-slate-500 border-4 rounded-md pl-3 hover:bg-neutral-100 focus:border-none" type='text' placeholder='Type ya message'/>
-        <button onClick={click} className="bg-sky-500 text-neutral-100 font-bold text-lg border border-sky-600 hover:border-sky-700 border-4 rounded-md">SEND</button>
+        <button className="bg-sky-500 text-neutral-100 font-bold text-lg border border-sky-600 hover:border-sky-700 border-4 rounded-md">SEND</button>
       </div>
     </div>
   )
